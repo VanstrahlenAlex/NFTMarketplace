e1b5fffcec991aacb68a6cc29288df5ca791a22d
Alexander Van strahlen <75139319+VanstrahlenAlex@users.noreply.github.com>
2026-06-08 17:23:53 -0500

    feat: initialize NFTMarketplace project with Foundry and implement NFT listing logic

---
+++ src/NFTMarketplace.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.35;
+
+import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
+
+
+contract NTFMarketplace is Ownable{
+
+
+	struct Listing {
+		address seller;
+		address nftAddress;
+		uint256 tokenId;
+		uint256 price;
+	}
+
+	mapping(address => mapping(uint256 => Listing) ) listing;
+
+
+	constructor() Ownable(msg.sender) {
+		
+
+	}
+
+
+	// Listar NFTs
+	function listNFT(address nftAddress_, uint256 tokenId_, uint256 price_) external { 
+
+		require(price_ > 0, "Price can not be 0");
+
+		address owner_ = IERC721(nftAddress_).ownerOf(tokenId_);
+		require(owner_ == msg.sender, "You are not the owner of this NFT");
+		
+
+		Listing memory listing_ = Listing({
+			seller: msg.sender,
+			nftAddress: nftAddress_,
+			tokenId: tokenId_,
+			price: price_
+		});
+
+		listing[nftAddress_][tokenId_] = listing_;
+		
+		
+	}
+
+
+	//Buy NFTs
+
+
+	//Cancel List 
+	function cancelList(address nftAddress_, uint256 tokenId_, uint256 price_) external { 
+		
+	}
+}
